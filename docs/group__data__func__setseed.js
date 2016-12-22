ba029c365b788fd7539f7b1bd6de8781c04151d6
Shehzan Mohammed <shehzan@arrayfire.com>
2016-12-22 10:40:40 -0500

    Update documentation to v3.4.1

--- docs/group__data__func__setseed.js
+++
@@ -1,5 +0,0 @@
-var group__data__func__setseed =
-[
-    [ "af_set_seed", "group__data__func__setseed.htm#gacf9fadb0a9d3b094660b5d33eef433f7", null ],
-    [ "setSeed", "group__data__func__setseed.htm#ga627b3f73960b7f57ac376c77f2cce98b", null ]
-];
--- docs/group__index__func__util.js
+++
@@ -1,8 +0,0 @@
-var group__index__func__util =
-[
-    [ "af_create_indexers", "group__index__func__util.htm#ga3e10cf2a97a2880b5872ec658af4169f", null ],
-    [ "af_release_indexers", "group__index__func__util.htm#gac382e89701583a173935262e741d4c7d", null ],
-    [ "af_set_array_indexer", "group__index__func__util.htm#gae109f51eb0b4fcfcbf5d10c737355f40", null ],
-    [ "af_set_seq_indexer", "group__index__func__util.htm#gaec0df3f1ee8624870f6bec55f6ec4bad", null ],
-    [ "af_set_seq_param_indexer", "group__index__func__util.htm#ga84fa7d63b756329b02d03cc56c65f699", null ]
-];
